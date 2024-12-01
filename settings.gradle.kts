26778826675855f60e49e9bad41d1c96057c5c34
koeltv <Valentin.koeltgen@utt.fr>
2024-12-01 18:25:29 +0100

    Updated dependencies & added version control plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,13 @@
 rootProject.name = "advent-of-code"
 
+plugins {
+    // See https://splitties.github.io/refreshVersions
+    id("de.fayard.refreshVersions") version "0.60.5"
+}
+
+refreshVersions {
+    rejectVersionIf {
+        @Suppress("UnstableApiUsage")
+        candidate.stabilityLevel.isLessStableThan(current.stabilityLevel)
+    }
+}
