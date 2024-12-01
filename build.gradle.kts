c7872e5c7d57fb69b213ca9698413e16cdde2334
koeltv <Valentin.koeltgen@utt.fr>
2024-12-01 19:32:47 +0100

    Migrating tasks to be configuration cache compatible

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ dependencies {
 }
 
 jmh {
-    properties["select"]?.toString()
+    providers.gradleProperty("select").orNull
         ?.split(',')
         ?.map { ".*$it.*" }
         ?.let { includes = it }
@@ -33,81 +33,74 @@ kotlin {
     jvmToolchain(21)
 }
 
-val sourceDirectory = File("./src/main/kotlin")
+val templatePaths = listOf(
+    "source" to "src/main/kotlin/y\$year/Day\$dayNumber.kt",
+    "test" to "src/test/kotlin/y\$year/Day\$dayNumberTest.kt",
+    "input" to "src/main/resources/y\$year/Day\$dayNumber.txt",
+    "input" to "src/main/resources/y\$year/Day\$dayNumber_test.txt",
+)
 
-// Setup everything for the current year
+val optionalTemplatePaths = listOf(
+    "benchmark" to "src/jmh/kotlin/y\$year/Day\$dayNumberBenchmark.kt",
+)
+
+val yearProperty = providers.gradleProperty("year")
+val dayProperty = providers.gradleProperty("day")
+
 tasks.register("newYear") {
-    doLast {
-        val newYear =
-            if (project.hasProperty("year")) getYear()?.toInt() ?: error("Wrong format for year (should be YYYY)")
-            else LocalDate.now().year
+    description = "Creates the directories for an AOC year"
+    group = "Advent of Code"
 
-        if (File("./src/main/kotlin/y$newYear").exists()) {
+    val newYear = getYear()
+
+    val sourceDir = layout.projectDirectory.dir("src/main/kotlin/y$newYear").asFile
+    val testDir = layout.projectDirectory.dir("src/test/kotlin/y$newYear").asFile
+    val ressourceDir = layout.projectDirectory.dir("src/resources/kotlin/y$newYear").asFile
+
+    doLast {
+        if (sourceDir.exists()) {
             println("$newYear was already setup")
         } else {
-            File("./src/main/kotlin/y$newYear").mkdir()
-            File("./src/test/kotlin/y$newYear").mkdir()
-            File("./src/resources/kotlin/y$newYear").mkdir()
+            sourceDir.mkdir()
+            testDir.mkdir()
+            ressourceDir.mkdir()
 
             println("Ready for $newYear !")
         }
     }
 }
 
-val templatePaths = listOf(
-    "source" to "src/main/kotlin/y\$year/Day\$dayNumber.kt",
-    "test" to "src/test/kotlin/y\$year/Day\$dayNumberTest.kt",
-    "input" to "./src/main/resources/y\$year/Day\$dayNumber.txt",
-    "input" to "./src/main/resources/y\$year/Day\$dayNumber_test.txt",
-)
-
-val optionalTemplatePaths = listOf(
-    "benchmark" to "./src/jmh/kotlin/y\$year/Day\$dayNumberBenchmark.kt",
-)
-
-// Setup everything for a new day (source file, test file, empty input files)
 tasks.register("newDay") {
+    description = "Creates all the necessary files for a new AOC day from the templates (sources, tests, input files)"
+    group = "Advent of Code"
+
+    val yearDirectory = getCurrentYearDirectory()
+    val currentDay = getNextDay(yearDirectory)
+
+    val properties = providers.gradlePropertiesPrefixedBy("").get()
+    val selectedTemplatePaths = templatePaths + optionalTemplatePaths.filter { (template, _) -> template in properties }
+
+    val currentYear = yearDirectory.name.drop(1)
+
+    val templateMap = selectedTemplatePaths.associate { (template, pathTemplate) ->
+        val templateFile = layout.projectDirectory.file("templates/$template.txt").asFile
+        val newFileName = pathTemplate.replace("\$year", currentYear).replace("\$dayNumber", currentDay)
+
+        templateFile to layout.projectDirectory.file(newFileName).asFile
+    }
+
     doLast {
-        val yearDirectory = if (project.hasProperty("year")) {
-            File("src/main/kotlin/y${getYear()!!}")
-        } else {
-            sourceDirectory.listFiles()!!
-                .filter { Regex("y(\\d{4})").matches(it.name) }
-                .maxByOrNull { it.name }
-                ?: error("No package found with the format \"yYYYY\" (ex: \"y2023\")")
-        }
-
-        val currentYear = yearDirectory.name.drop(1)
-
-        val currentDay = if (project.hasProperty("day")) {
-            getDay() ?: error("Wrong format for day (should be between 1 and 25)")
-        } else {
-            yearDirectory.listFiles()!!.let { directory ->
-                if (directory.isEmpty()) "01"
-                else directory.mapNotNull { Regex("Day(\\d{2})").find(it.name)?.groupValues?.get(1)?.toInt() }
-                    .maxOf { it + 1 }
-                    .let { "$it".padStart(2, '0') }
-            }
-        }
-
         if (currentDay.toInt() > 25) {
             println("Cannot create a new day. Maximum number of days (25) reached.")
             return@doLast
         }
 
-        val selectedTemplatePaths =
-            templatePaths + optionalTemplatePaths.filter { (template, _) -> properties.contains(template) }
+        templateMap.forEach { (template, pathTemplate) ->
+            val filledTemplate = template.readText().replace("\$year", currentYear).replace("\$dayNumber", currentDay)
 
-        selectedTemplatePaths.forEach { (template, directory) ->
-            val templateFile = File("./templates/$template.txt")
-                .readText()
-                .replace("\$year", currentYear)
-                .replace("\$dayNumber", currentDay)
-
-            val newFile = directory.replace("\$year", currentYear).replace("\$dayNumber", currentDay)
-            File(newFile).apply {
+            pathTemplate.apply {
                 parentFile.mkdirs()
-                writeText(templateFile)
+                writeText(filledTemplate)
             }
         }
 
@@ -115,40 +108,91 @@ tasks.register("newDay") {
     }
 }
 
-// Create benchmark with a year and/or day passed
 tasks.register("newBenchmark") {
-    doLast {
-        val year = getYear() ?: error("You need to specify a year via -Pyear")
-        val day = getDay() ?: error("You need to specify a day via -Pday")
+    description = "Creates a benchmark from a template for the given day and year"
+    group = "Advent of Code"
 
-        if (!File("src/main/kotlin/y$year/Day$day.kt").exists()) {
+    val year = getYear()
+    val day = if (dayProperty.isPresent) getDay() else getCurrentDay(getCurrentYearDirectory())
+
+    val sourceFile = layout.projectDirectory.file("src/main/kotlin/y$year/Day$day.kt").asFile
+
+    val (template, newFile) = optionalTemplatePaths
+        .first { (template, _) -> template == "benchmark" }
+        .let { (template, pathTemplate) ->
+            val templateFile = layout.projectDirectory.file("templates/$template.txt").asFile
+            val newFile = pathTemplate.replace("\$year", year).replace("\$dayNumber", day)
+
+            templateFile to layout.projectDirectory.file(newFile).asFile
+        }
+
+    doLast {
+        if (!sourceFile.exists()) {
             error("There is no source file for Day $day of year $year")
         }
 
-        optionalTemplatePaths
-            .first { (template, _) -> template == "benchmark" }
-            .let { (template, directory) ->
-                val templateFile = File("./templates/$template.txt")
-                    .readText()
-                    .replace("\$year", year)
-                    .replace("\$dayNumber", day)
+        val filledTemplate = template.readText().replace("\$year", year).replace("\$dayNumber", day)
 
-                val newFile = directory.replace("\$year", year).replace("\$dayNumber", day)
-                File(newFile).apply {
-                    parentFile.mkdirs()
-                    writeText(templateFile)
-                }
-            }
+        newFile.apply {
+            parentFile.mkdirs()
+            writeText(filledTemplate)
+        }
 
         println("Day n°$day benchmark of year $year is ready !")
     }
 }
 
-fun getYear(): String? = properties["year"]
-    ?.toString()
-    ?.let { Regex("(\\d{4})").find(it)?.destructured?.component1() }
+fun getYear(): String {
+    return if (yearProperty.isPresent) {
+        yearProperty.get()
+            .let { Regex("(\\d{4})").find(it)?.destructured?.component1() }
+            ?: error("Year should be in the format <YYYY>, ex: -Pyear=2023")
+    } else {
+        LocalDate.now().year.toString()
+    }
+}
 
-fun getDay(): String? = properties["day"]
-    ?.toString()
-    ?.let { Regex("([1-9]|(1[0-9])|(2[0-5]))").find(it)?.destructured?.component1() }
+fun getDay(): String = dayProperty.get()
+    .let { Regex("([1-9]|(1[0-9])|(2[0-5]))").find(it)?.destructured?.component1() }
     ?.padStart(2, '0')
+    ?: error("Day should be between 1 and 25, ex: -Pday=1")
+
+fun getCurrentDay(yearDirectory: File): String = if (dayProperty.isPresent) {
+    getDay()
+} else {
+    if (!yearDirectory.exists()) "01"
+    else {
+        yearDirectory.listFiles()!!.let { directory ->
+            if (directory.isEmpty()) "01"
+            else directory
+                .mapNotNull { Regex("Day(\\d{2})").find(it.name)?.groupValues?.get(1)?.toInt() }
+                .max()
+                .let { "$it".padStart(2, '0') }
+        }
+    }
+}
+
+fun getNextDay(yearDirectory: File): String {
+    return if (!yearDirectory.exists()) "01"
+    else {
+        yearDirectory.listFiles()!!.let { directory ->
+            if (directory.isEmpty()) "01"
+            else getCurrentDay(yearDirectory).toInt().let { "${it + 1}".padStart(2, '0') }
+        }
+    }
+}
+
+fun getCurrentYearDirectory(): File {
+    val sourceDir = layout.projectDirectory.dir("src/main/kotlin")
+    val year = getYear()
+
+    return if (yearProperty.isPresent) {
+        sourceDir.dir("y$year").asFile
+    } else {
+        sourceDir.asFile
+            .listFiles()!!
+            .filter { Regex("y(\\d{4})").matches(it.name) }
+            .maxByOrNull { it.name }
+            ?: sourceDir.dir("y$year").asFile
+    }
+}
